b9754db2010385af86ed8ed3b76bde431fbd8e35
Tanita Kidane <58995010+Tanitakidane@users.noreply.github.com>
2020-06-29 12:07:17 -0400

    Updates

---
+++ precache-manifest.c15292bf2cb45857b7def50f9df14ec2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534e316c54e279aca879b6fba3d4c3a0",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "4af888264c67e311ee2b",
+    "url": "/Clicky-Game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3bb04ea385d95d85a47b",
+    "url": "/Clicky-Game/static/js/2.34897f9f.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/Clicky-Game/static/js/2.34897f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af888264c67e311ee2b",
+    "url": "/Clicky-Game/static/js/main.af520d79.chunk.js"
+  },
+  {
+    "revision": "dde859553759047689d3",
+    "url": "/Clicky-Game/static/js/runtime-main.0c4a12cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
